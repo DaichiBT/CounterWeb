35d257fb8af81efb5f6d5e9444d5c4857958fb0e
DaichiBT <144473574+DaichiBT@users.noreply.github.com>
2024-10-17 23:41:55 -0400

    Add files via upload
    Typeeee

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 /* Counter Project */
-var numPeople = 0
-let counter = document.getElementById("count-el")
-let addButton = document.getElementById("increment-btn")
+var numPeople = 0;
+var totalPeople = 0;
+let counter = document.getElementById("count-el");
+let savedEntries = document.getElementById("saved-entries");
+let totalCounter = document.getElementById("total-count-el");
 
-updateDisplay()
+updateDisplay();
 
 function increment() {
-    numPeople++
-    updateDisplay()
+    numPeople++;
+    totalPeople++;
+    animateCounter();
+    updateDisplay();
 }
 
 function reset() {
-    numPeople = 0
-    updateDisplay()
+    numPeople = 0;
+    updateDisplay();
 }
 
 function updateDisplay() {
-    counter.innerText = numPeople
-    console.log(numPeople)
+    counter.innerText = numPeople;
+    totalCounter.innerText = totalPeople;
+}
+
+function save() {
+    let countStr = numPeople + " - ";
+    savedEntries.textContent += countStr;
+}
+
+function animateCounter() {
+    counter.classList.add("pop");
+    setTimeout(function() {
+        counter.classList.remove("pop");
+    }, 200);
 }
